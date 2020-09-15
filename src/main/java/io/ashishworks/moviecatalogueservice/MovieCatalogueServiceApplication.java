3dbb1b2184787c3ad6cf6a1bd498265d869efad8
ashishpathak128 <pathakashish128@yahoo.com>
2020-09-15 14:25:15 +0530

    Use the Web client instead of RestTemplate

--- src/main/java/io/ashishworks/moviecatalogueservice/MovieCatalogueServiceApplication.java
+++ src/main/java/io/ashishworks/moviecatalogueservice/MovieCatalogueServiceApplication.java
@@ -2,14 +2,26 @@ package io.ashishworks.moviecatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
 
 	public static void main(String[] args) {
 
-	    
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return new WebClient.builder();
+
+	}
 }
--- src/main/java/io/ashishworks/moviecatalogueservice/recurces/MovieCatalogueResource.java
+++ src/main/java/io/ashishworks/moviecatalogueservice/recurces/MovieCatalogueResource.java
@@ -3,11 +3,15 @@ package io.ashishworks.moviecatalogueservice.recurces;
 import io.ashishworks.moviecatalogueservice.models.CatologueItem;
 import io.ashishworks.moviecatalogueservice.models.Movie;
 import io.ashishworks.moviecatalogueservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.management.modelmbean.ModelMBeanNotificationInfo;
+import javax.management.monitor.MonitorNotification;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,15 +21,29 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogueResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientbuilder;
+
     @RequestMapping("/{userId}")
     public List<CatologueItem> getCatalogue(@PathVariable("userId") String userId){
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
+
         List<Rating> ratings = Arrays.asList(
            new Rating("1234",4),
                 new Rating("jabTakhaiJaan",3)
         );
         return ratings.stream().map(rating -> {
            Movie movie =  restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            /*Movie  movie = webClientbuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();*/
+
             return new CatologueItem(movie.getName(),"Sci-Fi",rating.getRating());
         })
                 .collect(Collectors.toList());
